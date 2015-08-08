e29448b245490cac2d09332960f1c3787e75ddfd
Rafael Cruz Teixeira <rafacteixeira@gmail.com>
2015-08-08 12:14:54 -0300

    commit bomba

--- src/main/java/com/puc/acme/listener/ConfiguraApp.java
+++ src/main/java/com/puc/acme/listener/ConfiguraApp.java
@@ -1,28 +1,30 @@
 package com.puc.acme.listener;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import com.puc.acme.factory.FactoryBanco;
-public class ConfiguraApp implements ServletContextListener  {
-	
-	  @Override
-		public void contextDestroyed(ServletContextEvent arg0) {
-			System.out.println("Derrubando a aplicação");
+
+public class ConfiguraApp implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		System.out.println("Derrubando a aplicaÃ§Ã£o");
+	}
+
+	// Run this before web application is started
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		System.out.println("Iniciando a aplicaÃ§Ã£o");
+		try {
+			if (FactoryBanco.getInstance(arg0.getServletContext().getInitParameter("jndiBanco")).isInited()) {
+				System.out.println("Banco iniciado");
+			} else {
+				System.out.println("Erro ao iniciar banco");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
-	 
-	      //Run this before web application is started
-	    @Override
-	    public void contextInitialized(ServletContextEvent arg0) {
-	           System.out.println("Iniciando a aplicação");
-	        try {
-	            if (FactoryBanco.getInstance(arg0.getServletContext().getInitParameter("jndiBanco")).isInited()){
-	                    System.out.println("Banco iniado");    
-	            }else{
-	                System.out.println("Erro ao iniciar banco");    
-	            }
-	        } catch (Exception ex) {
-	           ex.printStackTrace();
-	        }
-	    }
+	}
 
 }
